d5e3a671c07977988beaff9a0a29113bdc0ba334
Patrik Kernstock <info@pkern.at>
2018-04-26 12:48:29 +0200

    Some general PS improvements in HIBP script

--- powershell/checkDocumentPasswordsHIBP/checkDocumentPasswordsHIBP.ps1
+++ powershell/checkDocumentPasswordsHIBP/checkDocumentPasswordsHIBP.ps1
@@ -31,11 +31,11 @@
   C:\PS> .\checkDocumentPasswordsHIBP.ps1 -File "servers.rtsz" -EncryptionPassword "EncryptionP@ssw0rd" -LockdownPassword "LockdownP@ssw0rd"
 .NOTES
   Name:           checkDocumentPasswordsHIBP
-  Version:        0.9.9-beta
+  Version:        0.1.0-beta
   Author:         Patrik Kernstock
-  Copyright:      (C) 2018 code4ward.net GmbH
+  Copyright:      (C) 2018 code4ward GmbH
   Creation Date:  April 25, 2018
-  Modified Date:  April 25, 2018
+  Modified Date:  April 26, 2018
   Changelog:      For exact script changelog please check out the git commits history at:
                   https://github.com/royalapplications/scripts/commits/master/powershell/checkDocumentPasswordsHIBP/checkDocumentPasswordsHIBP.ps1
   Support:        For support please check out the "Support" section in the README file here:
@@ -45,12 +45,16 @@
   https://github.com/royalapplications/scripts/commits/master/powershell/checkDocumentPasswordsHIBP/
 #>
 
+# TODO: Rename script to Check-DocumentPasswordsHIBP to be more PS-name-like
+
 ###################################
 ### SOME PRE-MAGIC CODE STARTS HERE ###
 ###################################
 
 ## PARAMETERS
 param(
+    [CmdletBinding()]
+
     # File path to document. Default: None.
     [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
     [String] $File,
@@ -69,29 +73,23 @@ param(
 # Path to the PowerShell module within the Royal TS installation directory (if it was installed elsewhere)
 $RoyalPowerShellModule = Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath 'code4ward.net\Royal TS V4\RoyalDocument.PowerShell.dll'
 
-####################################
-### ** THE MAGIC STARTS HERE! ** ###
-####################################
-##  DO NOT TOUCH ANYTHING BELOW!  ##
-####################################
-
 # load modules: Royal TS.
 Import-Module $RoyalPowerShellModule
 
 # module loaded checks
 if (!(Get-Module "RoyalDocument.PowerShell")) {
-    Write-Host "RoyalDocument module not loaded."
-    Write-Host "Be sure Royal TS is installed and check if RoyalPowerShellModule is set."
-    Write-Host "Aborting."
+    Write-Error "RoyalDocument module not loaded. Be sure Royal TS is installed and check if RoyalPowerShellModule is set. Aborting." -Category NotImplemented
     exit
 }
 
 # sanity checks
 if (!(Test-Path $File)) {
-    Write-Host "Royal Document '$File' does not exist. Please provide a existing file. Aborting."
+    Write-Error "Royal Document '$File' does not exist. Please provide a existing file. Aborting." -Category OpenError
     exit
 }
 $RoyalDocFile = $File
+
+# TODO: no password provided? We need one! Check if Enc/Lockdown pwd required?
 
 # check if Encryption and Lockdown Password as Secured Strings
 if ($EncryptionPassword -ne $null -and $EncryptionPassword -isnot [SecureString]) {
@@ -166,7 +164,7 @@ function CheckPasswordWithHIBP()
 }
 
 # prepare some stuff.
-Write-Host -Verbose "+ Preparing..."
+Write-Verbose "+ Preparing..."
 # create store (container for any documents)
 $store = New-RoyalStore -UserName "HIBP-Checker"
 
@@ -176,15 +174,15 @@ $docHashedPasswords = @{}
 $docObjNames = @{}
 
 # open document
-Write-Host -Verbose "+ Loading document..."
+Write-Verbose "+ Loading document..."
 $doc = Open-RoyalDocument -Store $store -FileName $RoyalDocFile -Password $EncryptionPassword -LockdownPassword $LockdownPassword
 # check if loading worked
 if ($doc -eq $null) {
-    Write-Host "Failed loading document. Please check. Aborting."
+    Write-Error -Message "Failed loading document. Please check. Aborting." -Category OpenError
     exit
 }
 
-Write-Host -Verbose "+ Loading credentials..."
+Write-Verbose "+ Loading credentials..."
 # get all objects with passwords
 $passwords = $doc.GetAllPasswordProperties()
 $passwords | ForEach-Object {
@@ -192,7 +190,10 @@ $passwords | ForEach-Object {
     $obj = $_.Item1
     # get the cleartext password
     $clearPwd = $obj.GetPropertyValue($_.Item2)
-    if ($clearPwd -ne "") {
+    # check if passwords are not empty and are not any CustomProperties
+    # INFO: CustomProperties can not be iteriated with PS-API as of now
+    # TODO: REMEMBER OBJECT PROPERTY NAME
+    if ($clearPwd -ne "" -and $_.Item2.Name -ne "CustomProperties") {
         # remember name for later result list
         $docObjNames[$obj.ID] = $obj.Name
         # now only save hashed password into memory
@@ -214,11 +215,12 @@ $passwords = $null
 # output some stats
 $totalPasswords = $docHashedPasswords.get_Count()
 $totalObjects = $docObjNames.get_Count()
-Write-Host -Verbose "Total of unique passwords to check: $totalPasswords"
-Write-Host -Verbose "Total of objects with passwords found: $totalObjects"
+Write-Host "Total of unique passwords to check: $totalPasswords"
+Write-Host "Total of objects with passwords found: $totalObjects"
 
+# TODO: PROGRESS BAR
 # now we're checking the passwords...
-Write-Host -Verbose "+ Checking passwords against HIBP..."
+Write-Verbose "+ Checking passwords against HIBP..."
 $totalMatches = 0
 $hashedPwdCount = 0
 $docHashedPasswords.Keys | ForEach-Object {
@@ -228,7 +230,7 @@ $docHashedPasswords.Keys | ForEach-Object {
     $hashMatches = CheckPasswordWithHIBP -stringHash $_
     # if we do not get any integer back, anything wrong happend.
     if ($hashMatches -isnot [int]) {
-        Write-Host " Error ocurred while checking: $hashMatches"
+        Write-Error -Message " Error ocurred while checking: $hashMatches" -Category InvalidResult
         continue
     }
 
@@ -247,7 +249,7 @@ $docHashedPasswords.Keys | ForEach-Object {
 }
 
 # ...and we're done.
-Write-Host -Verbose "+ Password check done."
+Write-Verbose "+ Password check done."
 if ($totalMatches -gt 0) {
     Write-Host "In total your passwords has been pwned ${totalMatches}x." -ForegroundColor Red
     Write-Host "Changing affected passwords is strongly recommended!" -ForegroundColor Red
@@ -256,6 +258,6 @@ if ($totalMatches -gt 0) {
 }
 
 # ...and we're done.
-Write-Host -Verbose "+ Done."
+Write-Verbose "+ Done."
 
 # FIN
