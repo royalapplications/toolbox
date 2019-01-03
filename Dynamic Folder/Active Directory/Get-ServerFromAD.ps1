109bca54e5f40d4e6d5bba9788af0a4fe457aa28
Jan Egil Ring <janegilring@users.noreply.github.com>
2019-01-03 06:53:29 +0100

    Adds Dynamic Folder Scripts for retrieving computer names from Active Directory and System Center Virtual Machine Manager

---
+++ Dynamic Folder/Active Directory/Get-ServerFromAD.ps1
@@ -0,0 +1,108 @@
+function Get-ServerFromAD {
+    <#
+      .SYNOPSIS
+      Get-ServerFromAD is a command to retrieve server information from Active Directory.
+      .DESCRIPTION
+      Get-ServerFromAD is a command to retrieve server information from Active Directory.
+
+      You need to install Remote Server Administration Tools (RSAT) in order to leverage the Active Directory module for PowerShell from a workstation. Starting with Windows 10 October 2018 Update, RSAT is included as a set of "Features on Demand" in Windows 10 itself. From PowerShell, it can be installed using this command:
+      Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+
+
+           Required version: Windows PowerShell 3.0 or later 
+           Required modules: ActiveDirectory
+           Required privileges: Read-permission in AD
+
+      .LINK https://www.powershellmagazine.com/?p=13184
+      .EXAMPLE
+      Get-ServerFromAD
+      .EXAMPLE
+      Export data to Excel (requires the ImportExcel module)
+      $XlsxPath = 'C:\temp\Servers_AD_InventoryReport.xlsx'
+      Get-ServerFromAD | 
+      Export-Excel -Path $XlsxPath -WorkSheetname Servers -AutoSize -TableName Servers -TableStyle Light1
+  #>
+
+    [CmdletBinding()]
+    Param(
+        [string]$InactiveComputerObjectThresholdInDays = 30,
+        [string]$RootOUPath,
+        [string]$ADDomainController,
+        [pscredential]$ADCredential
+    )
+
+    try {
+        
+        Import-Module -Name ActiveDirectory -ErrorAction Stop -WarningAction SilentlyContinue
+        
+    } catch {
+        
+        Write-Error -Message 'Prerequisites missing (ActiveDirectory module not installed)'
+        break
+        
+    }
+
+    $Parameters = @{}
+
+    $null = $Parameters.Add('LDAPFilter', "(&(objectCategory=computer)(operatingSystem=Windows Server*)(!serviceprincipalname=*MSClusterVirtualServer*))")
+
+    $ADProperty = 'name', 'operatingSystem', 'lastlogondate', 'description', 'DistinguishedName', 'CanonicalName'
+    $null = $Parameters.Add('Properties', $ADProperty)
+
+    if ($ADCredential) {
+
+        $null = $Parameters.Add('Credential', $ADCredential)
+
+    }
+
+    if ($RootOUPath) {
+
+        $null = $Parameters.Add('SearchBase', $RootOUPath)
+
+    }
+
+    if ($ADDomainController) {
+
+        $null = $Parameters.Add('Server', $ADDomainController)
+
+    }
+
+    Get-ADComputer @Parameters |
+        Where-Object lastlogondate -gt (Get-Date).AddDays( - $InactiveComputerObjectThresholdInDays) |  
+        Select-Object -Property $ADProperty |
+        Sort-Object -Property name
+        
+}
+
+$ADCredentialPath = "$env:HOMEPATH\AD.cred.xml"
+
+if (Test-Path -Path $ADCredentialPath) {
+
+    $ADCredential = Import-Clixml -Path $ADCredentialPath
+
+} else {
+
+    $ADCredential = Get-Credential -Message 'Specify AD credentials'
+    $ADCredential | Export-Clixml -Path $ADCredentialPath
+
+}
+
+[System.Collections.ArrayList]$Servers = @()
+
+Get-ServerFromAD -InactiveComputerObjectThresholdInDays 14 -ADDomainController powershell.no -ADCredential $ADCredential | ForEach-Object {
+
+    $null = $Servers.Add([PSCustomObject]@{
+        Name = $PSItem.Name
+        Type = 'RemoteDesktopConnection'
+        ComputerName = $PSItem.Name
+        #CredentialName = 'DOMAIN\username'
+        Path = $PSItem.CanonicalName.Replace("/$($PSItem.Name)",'')
+    })
+
+} 
+
+$RoyalTSObjects = @{}
+$null = $RoyalTSObjects.Add('Objects',$Servers)
+
+
+$RoyalTSObjects | ConvertTo-Json
---
+++ Dynamic Folder/System Center Virtual Machine Manager/Get-ServerFromSCVMM.ps1
@@ -0,0 +1,115 @@
+function Get-ServerFromSCVMM {
+    <#
+       .SYNOPSIS
+       Get-ServerFromSCVMM is a command to retrieve server information from System Center Virtual Machine Manager.
+       .DESCRIPTION
+       Get-ServerFromSCVMM is a command to retrieve server information from System Center Virtual Machine Manager.
+
+            Required version: Windows PowerShell 3.0 or later 
+            Required modules: VirtualMachineManager
+            Required privileges: Read-permission in SC VMM
+
+       .LINK https://www.powershellmagazine.com/?p=13184
+       .EXAMPLE
+       Get-ServerFromSCVMM -VMMServer SRV01 -Credential (Get-Credential)
+       .EXAMPLE
+       Export data to Excel (requires the ImportExcel module)
+       $XlsxPath = 'C:\temp\Servers_VMM_InventoryReport.xlsx'
+       Get-ServerFromSCVMM -VMMServer SRV01 -Credential (Get-Credential) | 
+       Export-Excel -Path $XlsxPath -WorkSheetname Servers -AutoSize -TableName Servers -TableStyle Light1
+   #>
+   [CmdletBinding()]
+    Param(
+        [PSCredential]$Credential = (Get-Credential),
+        [string]$VMMServer = 'SRV01'
+    )
+
+
+    $VMMData = @()
+
+    try {
+
+        Write-Verbose "Connecting to VMM Server $VMMServer"
+
+        $VMMPSSession = New-PSSession -ComputerName $VMMServer -Credential $Credential -ErrorAction Stop
+
+        Write-Verbose "Getting VM hosts"
+
+        $VMHosts = Invoke-Command -Session $VMMPSSession -ScriptBlock {
+
+            $null = Get-SCVMMServer -ComputerName $using:VMMServer
+            Get-SCVMHost | Select-Object @{n='Name';e={$_.ComputerName}}, @{n='IsVirtualMachine';e={$false}}, LogicalCPUCount, PhysicalCPUCount, @{Name = "MemoryInGB"; Expression = {"{0:N2}" -f ($_.TotalMemory / 1gb)}}, @{Name = "TotalStorageCapacityInGB"; Expression = {"{0:N2}" -f ($_.LocalStorageTotalCapacity / 1gb)}}, Operatingsystem
+        
+
+        } -ErrorAction Stop | Select-Object Name,IsVirtualMachine,LogicalCPUCount,PhysicalCPUCount,MemoryInGB,TotalStorageCapacityInGB,OperatingSystem,Description
+
+        $VMMData += $VMHosts
+
+        Write-Verbose "Found $($VMHosts.Count) VM hosts in VMM"
+
+        Write-Verbose  "Getting VMs"
+
+        $VMs = Invoke-Command -Session $VMMPSSession -ScriptBlock {
+
+            Get-SCVirtualMachine | 
+                Where-Object ReplicationMode -ne 'Recovery' | 
+                Where-Object {$PSItem.OperatingSystem.Name -notlike "*Windows XP*" -and $PSItem.OperatingSystem.Name -notlike "*Windows 7*" -and $PSItem.OperatingSystem.Name -notlike "*Windows 10*" -and $PSItem.OperatingSystem.Name -ne 'Unknown'} | 
+                Select-Object Name, @{n='IsVirtualMachine';e={$true}}, @{n = 'LogicalCPUCount'; e = {$PSItem.CPUCount}}, PhysicalCPUCount, @{Name = "MemoryInGB"; Expression = {"{0:N2}" -f ($_.Memory / 1kb)}}, @{Name = "TotalStorageCapacityInGB"; Expression = {"{0:N2}" -f ($_.TotalSize / 1gb)}}, Operatingsystem,Description
+      
+
+        } -ErrorAction Stop | Select-Object Name,IsVirtualMachine,LogicalCPUCount,PhysicalCPUCount,MemoryInGB,TotalStorageCapacityInGB,OperatingSystem,Description
+
+        $VMMData += $VMs
+
+        Write-Verbose "Found $($VMs.Count) VMs in VMM" 
+
+    }
+
+    catch {
+
+        Write-Error "An error occured during VMM operations: $($_.Exception.Message)"
+
+    }    
+
+    if ($VMMPSSession) {
+
+        $VMMPSSession | Remove-PSSession
+
+    }
+
+    $VMMData
+
+}
+
+$VMMCredentialPath = "$env:HOMEPATH\VMM.cred.xml"
+
+if (Test-Path -Path $VMMCredentialPath) {
+
+    $VMMCredential = Import-Clixml -Path $VMMCredentialPath
+
+} else {
+
+    $VMMCredential = Get-Credential -Message 'Specify VMM credentials'
+    $VMMCredential | Export-Clixml -Path $VMMCredentialPath
+
+}
+
+[System.Collections.ArrayList]$Servers = @()
+
+Get-ServerFromSCVMM -ComputerName VMM01.powershell.no -Credential $VMMCredential | ForEach-Object {
+
+    $null = $Servers.Add([PSCustomObject]@{
+        Name = $PSItem.Name
+        Type = 'RemoteDesktopConnection'
+        ComputerName = $PSItem.Name
+        CredentialName = 'DOMAIN\username'
+        Path = 'VMM'
+    })
+
+} 
+
+$RoyalTSObjects = @{}
+$null = $RoyalTSObjects.Add('Objects',$Servers)
+
+
+$RoyalTSObjects | ConvertTo-Json
