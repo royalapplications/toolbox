021593b7719f43f67b2c5763769375874dfc189b
nil4 <nil4@users.noreply.github.com>
2025-06-02 10:03:31 +0200

    [6/6] revert temporary/demo commits
    Revert "[5/6] demo `.rdfe` to `.rdfx` migration results"
    
    This reverts commit eb704aee8226a01f8c014d890588b68205c036c4.
    
    Revert "[4/6] illustrate auto-extracted script files"
    
    This reverts commit 3880737730ee023347b4337573e08c2df72b10b7.
    
    Revert "[3/6] illustrate auto-generated `README.md` files"
    
    This reverts commit 4677779386fdffc049bde201f164c6bc00fe3de3.
    
    Revert "[2/6] illustrate `index.json` changes"
    
    This reverts commit 86c236c47528b26cb09b5332ff9ee7fd3320f2aa.

--- Dynamic Folder/Passwordstate/Passwordstate - Based on management module (PowerShell).dynamicCredential.ps1
+++
@@ -1,5 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$ErrorActionPreference = "Stop"
-$results = Get-PasswordStatePassword -PasswordID "$DynamicCredential.EffectiveID$"
-$results.Password = $results.GetPassword()
-$results | Select-Object Username,Password | ConvertTo-Json -Depth 100 | Write-Output
